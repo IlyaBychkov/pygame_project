412d160d1623449878c141d95f80bdd6a7dcf2db
MrAnt1vit <vityuka05@mail.ru>
2021-12-21 22:03:47 +0300

    MrAnt1vit 21.12

--- codes/main.py
+++ codes/main.py
@@ -1,4 +1,33 @@
-import pygame, start
+import os
+import sys
+
+import pygame
+
+import start
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join(os.pardir, 'img', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        pass
+        # image = image.convert_alpha()
+    return image
+
 
 if __name__ == '__main__':
     pygame.init()
--- codes/start.py
+++ codes/start.py
@@ -1,17 +1,27 @@
 import pygame
+from codes.main import load_image, terminate
+from PIL import Image
 
 
 class Chess:
     def __init__(self):
-        self.size = self.weight, self.height = 500, 500
+        image = load_image('fon.png')
+        self.size = self.width, self.height = image.get_width() / 2, image.get_height() / 2
         self.screen = pygame.display.set_mode(self.size)
         pygame.display.set_caption("Добро пожаловать в игру")
+        self.fon = pygame.transform.scale(load_image('fon.jpg'), (self.width, self.height))
+        self.fps = 60
+        self.clock = pygame.time.Clock()
+        self.screen.blit(self.fon, (0, 0))
+        self.main()
+
+    def main(self):
         running = True
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-            self.screen.fill((0, 0, 0))
+            self.screen.blit(self.fon, (0, 0))
             pygame.display.flip()
         pygame.quit()
 
